916666e22282cb54aba94ab634352174935a148e
Ashrafi <ashrafikhanam.18@gmail.com>
2023-12-21 20:28:39 +0600

    form added

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,6 +1,48 @@
 <template>
-    <h2>Add Resource</h2>
+    <base-card>
+    <form @submit.prevent="submitData">
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" ref="titleInput">
+      </div>
+      <div class="form-control">
+        <label for="title">Description</label>
+       <textarea name="description" id="description" ref="descInput" cols="30" rows="3"></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input type="url" name="link" id="link" ref="linkInput">
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+    </base-card>
 </template>
+
+<script>
+export default {
+  inject: ['addResource'],
+  methods:{
+   
+    submitData(){
+       try{   
+      const enteredTitle = this.$refs.titleInput.value;
+      const enteredDescription = this.$refs.descInput.value;
+      const enteredUrl = this.$refs.linkInput.value;
+      this.addResource(enteredTitle, enteredDescription, enteredUrl);
+      }
+      catch(error){
+        console.error('Error in submitData:', error);
+      }
+  
+
+    },
+    },
+};
+
+</script>
+
 <style scoped>
 label {
   font-weight: bold;
@@ -27,7 +69,5 @@ textarea:focus {
 .form-control {
   margin: 1rem 0;
 }
-
-
 
 </style>
